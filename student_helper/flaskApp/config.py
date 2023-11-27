037f24807763da36d0027f39b7db034141f5f7dd
DmitriiKoptev <112893017+DmitriiKoptev@users.noreply.github.com>
2023-11-27 10:38:43 +0300

    Delete student_helper directory

--- student_helper/flaskApp/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    SECRET_KEY = '48ab4c45a40a50f03aa1c6cde8a9310e'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
--- student_helper/flaskApp/materials/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-mat = Blueprint('materials', __name__)
-
-@mat.route("/materials")
-def materials():
-    return render_template('materials.html')
